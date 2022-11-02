51d53c2881660e7ce7e9009ed39ed4a76ffa9841
Justin Deschamp <deschampjustin@gmail.com>
2022-11-02 12:43:42 +0000

    Use add_struct_by_ids rather than name to create structures

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,15 @@
 use proc_macro::TokenStream;
 use proc_macro2::TokenStream as TokenStream2;
-use quote::quote;
-use regex::Regex;
+use quote::{format_ident, quote};
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
 use syn::{parse_macro_input, DeriveInput, Type};
 
-/// This function converts syn's string representation of typenames to
-/// the format used by std::any::type_name.
-fn canonicalized_type_name(ty: &Type) -> String {
-    let type_name = quote! {#ty}.to_string();
-    let re = Regex::new(r"\s*;\s*").expect("Bad regex");
-    re.replace_all(&type_name, r"; ").to_string()
+/// Hashes a given value.
+fn calculate_hash<T: Hash>(t: &T) -> u64 {
+    let mut s = DefaultHasher::new();
+    t.hash(&mut s);
+    s.finish()
 }
 
 /// Recursively adds inner types to the inner_types list.
@@ -24,8 +24,9 @@ fn add_type(ty: &Type, inner_types: &mut Vec<TokenStream2>) {
         _ => (),
     }
 
+    let local_name = format_ident!("local_{}", calculate_hash(&ty));
     inner_types.push(quote! {
-        <#ty>::add_to_database(database)?;
+        let #local_name = <#ty>::add_to_database(database)?;
     });
 }
 
@@ -44,8 +45,8 @@ pub fn derive_add_to_database(input: TokenStream) -> TokenStream {
                 let field_name = field.ident.expect("Field has no name").to_string();
                 let ty = field.ty;
                 add_type(&ty, &mut inner_types);
-                let type_name = canonicalized_type_name(&ty);
-                fields.push(quote! {(#field_name, #type_name)});
+                let local_name = format_ident!("local_{}", calculate_hash(&ty));
+                fields.push(quote! {(#field_name, #local_name)});
             }
         }
         _ => panic!("Not a structure."),
@@ -54,9 +55,9 @@ pub fn derive_add_to_database(input: TokenStream) -> TokenStream {
     let gen = quote! {
         impl bpf_script::types::AddToTypeDatabase for #name {
             fn add_to_database(database: &mut bpf_script::types::TypeDatabase) -> bpf_script::error::Result<usize> {
-                const STRUCT_FIELDS: &[(&str, &str)] = &[#(#fields),*];
                 #(#inner_types)*
-                database.add_struct_by_names(Some(stringify!(#name)), STRUCT_FIELDS)
+                let struct_fields = [#(#fields),*].to_vec();
+                database.add_struct_by_ids(Some(stringify!(#name)), struct_fields.as_slice())
             }
         }
     };
